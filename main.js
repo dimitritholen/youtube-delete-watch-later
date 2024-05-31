c8e2cb4e2ebe0ec7da0fa66af14264cb415ff2ff
Dimitri Tholen <33127487+dimitritholen@users.noreply.github.com>
2024-05-31 23:01:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,8 @@ let timer = 500; //Number of milliseconds
 function delete_one(){
 	document.querySelector("ytd-playlist-video-renderer").querySelector("button").click();
 	setTimeout(()=>{
-		var options = document.querySelectorAll("paper-listbox")[1].querySelectorAll("ytd-menu-service-item-renderer");
-		if (options.length == 1){
-			options[0].click(); //If the video was removed from YT
-		} else {
-			options[2].click(); //If it's still there, the option
-				//to remove it is the third (unless they change it
-				//again to piss off their userbase further)
-		}
+		document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(3) > tp-yt-paper-item").click();
+		
 	}, timer);
 }
 
